10fcd42957a61553df14f71149e6cbba31cf14b5
Branden <branden.degrasse@uleth.ca>
2021-02-23 22:25:53 -0700

    started changing code on websites to different words based on what it detects

--- js/background.js
+++ js/background.js
@@ -1,7 +1,7 @@
  var audio = new Audio("note.mp3");
  audio.load();
 
-chrome.browserAction.onClicked.addListener(function(tab) {
+ chrome.browserAction.onClicked.addListener(function(tab) {
 	if(!audio.paused) audio.pause();
 	else {
 		audio.currentTime = 0;
--- js/content.js
+++ js/content.js
@@ -1,7 +1,63 @@
-console.log("extension is running!")
+walk(document.body);
 
-// TRY: updating the text content of an element or set of elements on one or more pages
+function walk(node) {
+  var child, next;
 
-// TRY: adding a click event listener to an element that changes the background of that element to a new color
+  if (node && node.isContentEditable) return;
 
-// TRY: using a mouseenter event listener to update text to something new.
+  switch (node.nodeType) {
+    case 1: // Element
+    case 9: // Document
+    case 11: // Document fragment
+      child = node.firstChild;
+      while (child) {
+        next = child.nextSibling;
+        walk(child);
+        child = next;
+      }
+      break;
+
+    case 3: // Text node
+      handleText(node);
+      break;
+  }
+}
+
+function handleText(textNode) {
+  var a = textNode.nodeValue;
+  if (a.match(/\baway\b/gi)) {
+    a = a
+      .replace(/\baway\b/g, "AFK")
+      .replace(/\bAway\b/g, "AFK");
+    textNode.nodeValue = a;
+  }
+    var b = textNode.nodeValue;
+  if (b.match(/\bhappy\b/gi)) {
+    b = b
+      .replace(/\bhappy\b/g, "POG")
+      .replace(/\bHappy\b/g, "POG");
+    textNode.nodeValue = b;
+  }
+}
+
+if (window.MutationObserver) {
+  var observer = new MutationObserver(function(mutations) {
+    mutations.forEach(function(mutation) {
+      if (mutation.addedNodes) {
+        for (var i = 0; i < mutation.addedNodes.length; i++) {
+          var node = mutation.addedNodes[i];
+          walk(node);
+        }
+      }
+    });
+  });
+
+  observer.observe(document.body, {
+    childList: true,
+    subtree: true
+  });
+}
+
+setTimeout(function() {
+  walk(document.body);
+}, 1000);
