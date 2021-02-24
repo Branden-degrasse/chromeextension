6213e74429018085d4c96cc407d5a94dd4037451
Branden <branden.degrasse@uleth.ca>
2021-02-23 21:39:02 -0700

    forgot to make repository

---
+++ js/background.js
@@ -0,0 +1,11 @@
+console.log("extension is running!")
+ var audio = new Audio("note.mp3");
+ audio.load();
+
+chrome.browserAction.onClicked.addListener(function(tab) {
+	if(!audio.paused) audio.pause();
+	else {
+		audio.currentTime = 0;
+		audio.play();
+		}
+});
