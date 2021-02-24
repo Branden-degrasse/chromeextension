38ce0aa0207e9375aee33b1baf19a7a3ba202763
Branden <branden.degrasse@uleth.ca>
2021-02-23 23:56:42 -0700

    added the ability to change all images to mine

--- js/content.js
+++ js/content.js
@@ -38,6 +38,45 @@ function handleText(textNode) {
       .replace(/\bHappy\b/g, "POG");
     textNode.nodeValue = b;
   }
+      var c = textNode.nodeValue;
+  if (c.match(/\blying\b/gi)) {
+    c = c
+      .replace(/\blying\b/g, "cap")
+      .replace(/\blie\b/g, "cap")
+      .replace(/\bLie\b/g, "Cap")
+      .replace(/\bLying\b/g, "Cap");
+    textNode.nodeValue = c;
+  }
+        var d = textNode.nodeValue;
+  if (d.match(/\bangry\b/gi)) {
+    d = d
+      .replace(/\bangry\b/g, "salt")
+      .replace(/\bAngry\b/g, "Salt");
+    textNode.nodeValue = d;
+  }
+        var e = textNode.nodeValue;
+  if (e.match(/\bsad\b/gi)) {
+    e = e
+      .replace(/\bsad\b/g, "feelsbadman")
+      .replace(/\bSad\b/g, "Feelsbadman");
+    textNode.nodeValue = e;
+  }
+          var f = textNode.nodeValue;
+  if (f.match(/\blaughing\b/gi)) {
+    f = f
+      .replace(/\blaughing\b/g, "OMEGALUL")
+      .replace(/\blaugh\b/g, "OMEGALUL")
+      .replace(/\bLaugh\b/g, "OMEGALUL")
+      .replace(/\bLaughing\b/g, "OMEGALUL");
+    textNode.nodeValue = f;
+  }
+          var g = textNode.nodeValue;
+  if (g.match(/\blaugh\b/gi)) {
+    g = g
+      .replace(/\blaugh\b/g, "OMEGALUL")
+      .replace(/\bLaugh\b/g, "OMEGALUL")
+    textNode.nodeValue = g;
+  }
 }
 
 if (window.MutationObserver) {
@@ -61,3 +100,8 @@ if (window.MutationObserver) {
 setTimeout(function() {
   walk(document.body);
 }, 1000);
+
+$("img").attr('src', 'https://i.redd.it/rzvf88ollt961.png').load(function(){
+    this.width;   // Note: $(this).width() will not work for in memory images
+
+});
